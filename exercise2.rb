8f42a7971ca604257bf3d246adfcb8925b240f36
Mike Cooper <shivagogo@gmail.com>
2015-10-27 16:19:50 -0400

    Exercise 3 - age

--- exercise2.rb
+++ exercise2.rb
@@ -1,4 +1,4 @@
-#1. tip = 11
+# #1. tip = 11
 meal = 55
 tip = 0.2
 puts "#{meal * tip}"
@@ -9,10 +9,10 @@ b = 2
 puts "#{a + b}" + 7.to_s
 
 
-# 3. 357677892
+# # 3. 357677892
 a = 45628
 b = 7839
 puts "The value of 45628 times 7839 is #{a * b}."
 
-#4. value = true
+# #4. value = true
 (true && false) || (false && true) || !(false && false)
