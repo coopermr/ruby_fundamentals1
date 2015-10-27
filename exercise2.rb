dadf2cf393a2428e43a91c43f666353a19bb1e7c
Mike Cooper <shivagogo@gmail.com>
2015-10-27 15:27:24 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+#1. tip = 11
+meal = 55
+tip = 0.2
+puts "#{meal * tip}"
+
+#2. convert integer with to_s
+a = 1
+b = 2
+puts "#{a + b}" + 7.to_s
+
+
+# 3. 357677892
+a = 45628
+b = 7839
+puts "The value of 45628 times 7839 is #{a * b}."
+
+#4. value = true
+(true && false) || (false && true) || !(false && false)
