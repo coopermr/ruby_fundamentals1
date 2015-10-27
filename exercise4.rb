6e91f295eaac5c8a262112a13becea5b60318ad4
Mike Cooper <shivagogo@gmail.com>
2015-10-27 17:30:39 -0400

    Exercise 4 - counter

--- exercise4.rb
+++ exercise4.rb
@@ -1,5 +1,12 @@
 one_to_hundred = (1..100)
 
 one_to_hundred.each do |number|
-	puts number
+if number % 15 == 0
+	puts "Bitmaker"
+elsif number % 5 == 0
+ 	puts "Maker"
+elsif number % 3 == 0
+	puts "Bit"
+else puts number
+end
 end
